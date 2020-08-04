68bb982b557fdffd83452abf98c76f92b5bcdedd
Jose Eduardo Kimura Reis <j.eduardokimura@icloud.com>
2020-08-04 15:32:17 +0200

    Add ls query

--- tempo.py
+++ tempo.py
@@ -1,12 +1,14 @@
+#!/usr/bin/env python3
 import click
 import sqlite3
 import json
 
 from collections import namedtuple
-from datetime import date
+from datetime import datetime, date, timedelta 
 
 
 Worklog = namedtuple('Worklog', ['description', 'time', 'issue', 'day'])
+conn = None
 cursor = None
 try:
     with open('config.json') as cfg:
@@ -17,9 +19,9 @@ try:
         cursor.execute("""CREATE TABLE IF NOT EXISTS worklogs (
                             id integer PRIMARY KEY,
                             description text NOT NULL,
-                            time integer NOT NULL,
+                            time float NOT NULL,
                             issue text NOT NULL,
-                            day text NOT NULL
+                            day date NOT NULL
                           );""")
 
         cfg.close()
@@ -42,8 +44,12 @@ def main():
 @click.argument('issue')
 @click.option('--day', '-d')
 def work(description, time, issue, day=None):
-    worklog = Worklog._make((description, time, issue, day or date.today().isoformat()))
-    cursor.execute('INSERT INTO worklogs (description, time, issue, day) VALUES {}'.format(tuple(worklog)))
+    try:
+        worklog = Worklog._make((description, time, issue, datetime.strptime(day, '%d-%m-%y') if day else date.today().isoformat()))
+        cursor.execute('INSERT INTO worklogs (description, time, issue, day) VALUES {};'.format(tuple(worklog)))
+        conn.commit()
+    except Error as e:
+        print(e)
 
 
 @main.command()
@@ -56,8 +62,17 @@ def send_month(month):
 
 @main.command()
 @click.option('--month', '-m')
-def ls(month):
-    pass
+@click.option('--today', '-t')
+def ls(month, today):
+    q = 'SELECT * FROM worklogs'
+    if month:
+        d = date(date.today().year, int(month), 1)
+        q = '{} WHERE day BETWEEN {} AND {}'.format(q, d, date(d.year, d.month+1, d.day) - timedelta(1))
+    q += ';'
+    print(q)
+    cursor.execute(q)
+    rows = cursor.fetchall()
+    print(rows)
 
 if __name__ == '__main__':
     main()
